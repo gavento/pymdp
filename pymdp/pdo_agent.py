0b909b7f3083c01217dd91ba52290566e4bbe37b
Tomas Gavenciak <gavento@gmail.com>
2024-08-23 13:02:29 +0000

    Initial implementation of PDO agent, missing: G impl

---
+++ pymdp/pdo_agent.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Agent Class
+
+__author__: Conor Heins, Alexander Tschantz, Daphne Demekas, Brennan Klein
+
+"""
+
+import itertools
+import warnings
+import jax
+import numpy as np
+from pymdp import inference, control, learning
+from pymdp import utils, maths
+import copy
+import logging
+import jax.numpy as jnp
+from pymdp.agent import Agent
+from pymdp.envs.env import Env
+from .pdo_policy import TabularPolicy, Policy, UniformPDOPolicy
+
+class PDOAgent(Agent):
+    """ 
+    The agent only runs inference on the first time it is called, then just follows the policy it inferred.
+    """
+
+    def __init__(self, A, B, time_horizon, env: Env, policy_iterations=1000, policy_lr=0.01, **kwargs):
+        super(PDOAgent, self).__init__(A, B, **kwargs)
+        self.policy = None
+        self.action = None
+        self.time_horizon = time_horizon
+        self.policy_iterations = policy_iterations
+        self.policy_lr = policy_lr
+        self.prior_policy = UniformPDOPolicy(action_counts=self.num_controls)
+        self.env = env
+
+    def infer_policies(self):
+        if self.policy is not None:
+            return  # Policy has already been inferred for this agent.
+        assert self.curr_timestep == 0
+
+        # Generate all possible observartion sequences. Each onbservation is a tuple of numbers
+        # Each of those numbers ranges in range(self.num_obs[i])
+        possible_observations = list(itertools.product(*[range(onum) for onum in self.num_obs]))
+        assert len(possible_observations) == np.prod(self.num_obs)
+        all_observation_seqs = tuple(itertools.product(possible_observations, repeat=self.time_horizon))
+        self.policy = TabularPolicy(action_counts=self.num_controls, observation_sequences=all_observation_seqs)
+
+        @jax.jit
+        def step_fn(policy):
+            gradG = jax.grad(self.G)(policy)
+            pol_table = policy.table - self.policy_lr * gradG.table
+            return policy.updated_copy(pol_table)
+        
+        logging.info(f"Running policy iteration:\n{0:03d}/{self.policy_iterations:03d}: G = {self.G(self.policy):.3f}")
+        for i in range(self.policy_iterations):
+            self.policy = step_fn(self.policy)
+            logging.info(f"{i+1:03d}/{self.policy_iterations:03d}: G = {self.G(self.policy):.3f}")
+
+    @jax.jit
+    def G(self, policy: Policy):
+        "This needs to be JAX-differentiable wrt values from `policy`"
+        return 0.0
+
+    def infer_states(self, observation, distr_obs=False):
+        assert distr_obs is False, "This agent does not support distributed observations."
+        assert self.curr_timestep == len(self.prev_obs)
+        self.prev_obs.append(tuple(observation))
+
+    def sample_action(self):
+        assert self.policy is not None, "No policy has been inferred for this agent."
+        self.action = self.policy.sample_action(tuple(self.prev_obs))
+        self.step_time()
+        return self.action
